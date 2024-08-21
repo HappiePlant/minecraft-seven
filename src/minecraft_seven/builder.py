f66ab6b424886458e17226e5ac1b800d0fefd296
HappiePlant <tijn.luijben@outlook.com>
2024-08-21 17:34:47 +0200

    Add image manipulation to builder

--- src/minecraft_seven/builder.py
+++ src/minecraft_seven/builder.py
@@ -1,6 +1,8 @@
+import io
 from zipfile import ZipFile
 import json
 from textwrap import wrap
+from PIL import Image
 
 def get_assets(jar_path: str) -> list[dict]:
     with ZipFile(jar_path, 'r') as jar:
@@ -12,19 +14,32 @@ def get_assets(jar_path: str) -> list[dict]:
             provider["file"] = jar.read(texture_path)
     return providers
 
-def build_tile_set(providers: list[dict]):
+def build_tileset(providers: list[dict]):
+    tileset = Image.new("RGBA", (10000, 12), color=(255, 0, 0, 0))
+    glyphs: str = ""
+    tileset_x = tileset_y = 0
     for provider in providers:
+        font_img = Image.open(io.BytesIO(provider["file"]))
+        font_x = font_y = 0
         if "height" in provider:
-            height = provider["height"]
+            char_height = provider["height"]
         else:
-            height = 8
+            char_height = 8
+        char_width = 8
         provider_chars: list[str] = provider["chars"]
-        for chars in provider_chars:
-            for char in chars:
+        for chars in provider_chars:  # "abcdefghij"
+            for char in chars:  # "a"
                 if char != "\x00":
-                    print(char)
+                    char_img = font_img.crop((font_x, font_y, font_x + char_width, font_y + char_height))
+                    tileset.paste(char_img, (tileset_x, tileset_y))
+                    glyphs += char
+                font_x += char_width
+                tileset_x += char_width
+            font_x = 0
+            font_y += char_height
+    tileset.save("out/tileset.png")
 
 
 def build_as_pixel_font_converter_zip(jar_path: str):
     providers = get_assets(jar_path)
-    build_tile_set(providers)
+    build_tileset(providers)
